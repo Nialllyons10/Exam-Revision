367e6cc6b7df8e7206b1403c2533f2f1fa1cbbd4
Niall Lyons <niall.lyons4@mail.dcu.ie>
2017-07-30 22:50:41 +0100

    Update Passwords.java

--- Passwords.java
+++ Passwords.java
@@ -3,17 +3,17 @@ import java.util.*;
 class Passwords
 {
 	public static void main(String[] args) 
-  {
+  	{
 		TreeMap<String, String> tm = new TreeMap<>();
 		String url, pw;
 		while(!Console.endOfFile())
-    {
+    		{
 			url = Console.readToken();
 			pw = Console.readToken();
 			if(tm.containsKey(url))
-      {
+      			{
 				if(pw.equals("?"))
-        {
+        			{
 					System.out.println(tm.get(url));
 				}
 				else
